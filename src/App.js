5615afc09361d47dde34a812cee8f849a04bb405
andrew <andrewshaky50@gamil.com>
2023-02-14 16:17:24 +0200

    modification

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React,{useEffect, useState,useMemo, useRef} from "react";
+import React,{useMemo, useRef} from "react";
 import { Route, Routes } from "react-router-dom";
-
 import './App.css'
 import { BermudaAwards } from "./components/bermudaAwards/berawards";
 import { FilmsPage } from "./components/films_page/filmspage";
 import { Help } from "./components/help/help";
 import { Home } from "./components/home/home";
-
 import { SearchBar } from "./components/main_layout/searchbar";
 import { SideBar} from "./components/main_layout/sidebar";
 import { Marathons } from "./components/marathons_page/marathon";
 import { Meme } from "./components/memes/meme";
 import { Threads } from "./components/threads/threads";
 import { TvShow } from "./components/tvshow_page/tvshow";
+
 export default function App(){
     let contentRef=useRef();
     let sidebar=useMemo(()=>{
@@ -29,7 +28,10 @@ return (
 <SearchBar contentRef={contentRef}/>
 <div className="display-content">
 <Routes>
-<Route path="/" element={<Home />}></Route>
+<Route  exact path="/">
+    
+</Route>
+<Route  path="/" element={<Home />}></Route>
 <Route path="/films" element={<FilmsPage />}></Route>
 <Route path="/tv-show" element={<TvShow/>}></Route>
 <Route path="/marathons" element={<Marathons />}></Route>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import './server';
 // import {unstable_HistoryRouter,BrowserRouter, HashRouter, MemoryRouter} from 'react-router-dom'
-// import {StaticRouter} from 'react-router-dom/server'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
